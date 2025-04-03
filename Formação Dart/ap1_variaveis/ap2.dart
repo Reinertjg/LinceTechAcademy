62f2bec06127f3501774b4ee0d763f77e0424723
Reinertjg <jgcosta.rei4@gmail.com>
2025-04-03 14:20:52 -0300

    Usando const na atividade ap2.dart

--- Formação Dart/ap1_variaveis/ap2.dart
+++ Formação Dart/ap1_variaveis/ap2.dart
@@ -4,16 +4,17 @@ import 'dart:io';
 void main() {
   List<double> numeros = []; // Criando uma lista vazia
   var media = 0.0;
+  const int totalNumeros = 4;
   double soma = 0.0;
 
-  for (int i =0; i < 4; i++) {
+  for (int i =0; i < totalNumeros; i++) {
     print("Informe o numero ${i + 1}");
     double numero = double.parse(stdin.readLineSync()!);
     numeros.add(numero);
     soma += numeros[i];
 
-    if (numeros.length == 4) {
-      media = soma / 4;
+    if (numeros.length == totalNumeros) {
+      media = soma / totalNumeros;
       print("A media entre $numeros = $media");
     }
   }
