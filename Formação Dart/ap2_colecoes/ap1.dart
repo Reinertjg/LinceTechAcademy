1b1834b2013ff78cfbd68b3112af9e20cdee01d8
Reinertjg <jgcosta.rei4@gmail.com>
2025-04-04 02:30:43 -0300

    Reestruturação atividade ap2_colecoes\ap1.dart

--- Formação Dart/ap2_colecoes/ap1.dart
+++ Formação Dart/ap2_colecoes/ap1.dart
@@ -2,12 +2,10 @@ import 'dart:math';
 
 void main() {
   List<int> numeros = []; // Criando uma lista vazia
-  const int totalNumeros = 10;
   final random = Random();
 
-  for (int i = 0; i < totalNumeros; i++) {
-    var numeroAleatorio = random.nextInt(100);
-    numeros.add(numeroAleatorio);
+  for (int i = 0; i < 10; i++) {
+    numeros.add(random.nextInt(100));
     print("Posição: $i, valor: ${numeros[i]}");
   }
 }
