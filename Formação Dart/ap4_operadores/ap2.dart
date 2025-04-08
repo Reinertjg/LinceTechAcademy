008801769e67b060383eb71f5345b9b8fef3c212
Reinertjg <jgcosta.rei4@gmail.com>
2025-04-08 16:50:56 -0300

    Realizado atividade ap3_operadores\ap2.dart

---
+++ Formação Dart/ap4_operadores/ap2.dart
@@ -0,0 +1,23 @@
+import 'dart:math';
+
+void main() {
+  List<String> listaString = [
+    '10',
+    '2XXL7',
+    'JOJ0',
+    '99',
+    '381',
+    'AD44',
+    '47',
+    '2B',
+    '123',
+    '78'
+  ];
+
+  print(converte(listaString));
+}
+
+List converte(List listaString) {
+  List<int> listaNumeros = [];
+  return listaString.map((elemento) => int.tryParse(elemento) ?? 0).toList();
+}
