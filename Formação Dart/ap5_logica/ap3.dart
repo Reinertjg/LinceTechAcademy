3c46aaa17c0b3be7b1bd503a9581dc6079184b0d
Reinertjg <jgcosta.rei4@gmail.com>
2025-04-10 20:52:57 -0300

    Realizada Atividade ap5_logica\ap3.dart

---
+++ Formação Dart/ap5_logica/ap3.dart
@@ -0,0 +1,21 @@
+import 'dart:math';
+
+void main() {
+  var random = Random();
+  // Gera uma numero aleatório de 100 ate 1000.
+  int numero = 100 + random.nextInt(1000 - 100 + 1);
+
+  // Imprime o resultado.
+  print("A soma dos números pares entre 0 e $numero é ${obterSomaPares(numero)}");
+}
+
+// Funcao que recebe um inteiro e retorna a soma de todos os números pares de 0 até esse número
+int obterSomaPares(int valor) {
+  int soma = 0;
+  for (int i = 0; i <= valor; i++) {
+    if (i.isEven) {
+      soma += i;
+    }
+  }
+  return soma;
+}
