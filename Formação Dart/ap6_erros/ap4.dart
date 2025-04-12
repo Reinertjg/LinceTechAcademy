6a651d8779ef030c91fe3eaa21922a2a006273b5
Reinertjg <jgcosta.rei4@gmail.com>
2025-04-12 15:35:34 -0300

    Realizada Atividade ap6_erros\ap4.dart

---
+++ Formação Dart/ap6_erros/ap4.dart
@@ -0,0 +1,41 @@
+
+void main() {
+  try {
+    // Definindo um nome arbitrário para o arquivo
+    final nomeArquivo = 'bloco.txt';
+
+    ArquivoTexto arquivoTexto = ArquivoTexto(nomeArquivo);
+    arquivoTexto.abrir();
+  } catch (e) {
+    // Imprimir mensagem de erro
+    print(e);
+  } finally {
+    print("Fim do programa");
+  }
+}
+
+// Interface abstrata chamada Arquivo
+// Toda classe que implementar essa interface deve implementar o metodo abrir()
+abstract class Arquivo {
+  void abrir();
+}
+
+// Classe que implementa a interface Arquivo
+class ArquivoTexto implements Arquivo {
+  // Construtor que recebe o nome do arquivo
+  ArquivoTexto(this.nome);
+
+  // Atributo final (imutável) com o nome do arquivo
+  final String nome;
+
+  // Implementação do método abrir
+  @override
+  void abrir() {
+    try {
+      throw Exception("Erro ao abrir o arquivo $nome");
+    } catch (e) {
+      print('Relançar a exception');
+      rethrow;
+    }
+  }
+}
