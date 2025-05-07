df4ea76d30fa3589aec384c1ee587f3691dfbdd0
João Guilherme <127997588+Reinertjg@users.noreply.github.com>
2025-05-06 22:00:02 -0300

    Realizado Interação IA - Introdução ao Flutter

---
+++ Formação Flutter/apf1_funcoes/ap2.dart
@@ -0,0 +1,87 @@
+import 'package:flutter/material.dart';
+import 'dart:math';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp(home: Jogo());
+  }
+}
+
+class Jogo extends StatefulWidget {
+  const Jogo({super.key});
+
+  @override
+  State<Jogo> createState() => _JogoState();
+}
+
+class _JogoState extends State<Jogo> {
+  Color corDeFundo = Colors.white;
+  int tentativa = 0;
+  int botaoCorreto = random.nextInt(3);
+  String mensagem = "Escolha um Botão!!";
+  
+  static get random => null;
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Container(
+        color: corDeFundo,
+        width: double.infinity,
+        height: double.infinity,
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Text(
+              mensagem,
+              style: const TextStyle(fontSize: 24, color: Colors.black),
+            ),
+            ElevatedButton(
+              onPressed: () {
+                validarTentativa(0);
+              },
+              child: const Text('Botão A'),
+            ),
+            const SizedBox(height: 16), // espaço entre os botões
+            ElevatedButton(
+              onPressed: () {
+                validarTentativa(1);
+              },
+              child: const Text('Botão B'),
+            ),
+            const SizedBox(height: 16), // espaço entre os botões
+            ElevatedButton(
+              onPressed: () {
+                validarTentativa(2);
+              },
+              child: const Text('Botão C'),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+
+
+
+  void validarTentativa(int botaoEscolhido) {
+    setState(() {
+      tentativa++;
+      if (botaoEscolhido == botaoCorreto) {
+        corDeFundo = Colors.green;
+        mensagem = 'Você acertou!';
+      } else if (tentativa >= 2) {
+        corDeFundo = Colors.red;
+        mensagem = 'Você perdeu!';
+        tentativa = 0;
+      }
+    });
+  }
+}
