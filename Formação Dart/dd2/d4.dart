c5d6f689deb7f89dddfceaf9ca065f7c47a540be
Reinertjg <jgcosta.rei4@gmail.com>
2025-05-17 10:59:35 -0300

    Adicionado projeto Dart - Desafio cronometrado #2 (d4.dart)

--- Formação Dart/dd2/d4.dart
+++ Formação Dart/dd2/d4.dart
@@ -1,145 +1,136 @@
 import 'dart:math' as math;
 
 void main() {
-  // Definindo o comparador de formas
   final comparador = ComparadorFormasGeometricas();
 
-  // Definindo as formas geometricas
-  final circuloA = Circulo('Circulo A', 3);
-  final circuloB = Circulo('Circulo B', 8);
-  final retanguloA = Retangulo('Retângulo A', 4, 3);
-  final retanguloB = Retangulo('Retângulo B', 19, 11);
+  final forma1 = Circulo('Circulo A', 3);
+  final forma2 = Retangulo('Retângulo B', 19, 11);
+  final forma3 = HexagonoRegular('Hexágono', 7);
+  final forma4 = TrianguloRetangulo('Triângulo Retângulo', 3, 4);
 
-  final circuloMaiorArea = comparador.circuloDeMaiorArea(circuloA, circuloB);
-  final retanguloMaiorArea = comparador.retanguloDeMaiorArea(
-    retanguloA,
-    retanguloB,
-  );
+  final maiorArea = comparador.maiorArea(forma1, forma2);
+  final maiorPerimetro = comparador.maiorPerimetro(forma3, forma4);
 
-  if (circuloMaiorArea.area > retanguloMaiorArea.area) {
-    print(
-      'A maior area e ${circuloMaiorArea.area.toStringAsFixed(2)} '
-          'e pertence a ${circuloMaiorArea.nome}',
-    );
-  } else {
-    print(
-      'A maior area e ${retanguloMaiorArea.area.toStringAsFixed(2)} '
-          'e pertence a ${retanguloMaiorArea.nome}',
-    );
-  }
-
-  final circuloaiorPerimetro = comparador.circuloDeMaiorPerimetro(
-    circuloA,
-    circuloB,
+  print(
+    'A maior área é ${maiorArea.area.toStringAsFixed(2)} e pertence a ${maiorArea.nome}',
   );
-  final retanguloMaiorPerimetro = comparador.retanguloDeMaiorPerimetro(
-    retanguloA,
-    retanguloB,
+  print(
+    'O maior perímetro é ${maiorPerimetro.perimetro.toStringAsFixed(2)} e pertence a ${maiorPerimetro.nome}',
   );
-  if (circuloaiorPerimetro.area > retanguloMaiorPerimetro.area) {
-    print(
-      'O maior perímetro e ${circuloaiorPerimetro.perimetro.toStringAsFixed(2)} '
-          'e pertence a ${circuloaiorPerimetro.nome}',
-    );
-  } else {
-    print(
-      'O maior perímetro e ${retanguloMaiorPerimetro.perimetro.toStringAsFixed(2)} '
-          'e pertence a ${retanguloMaiorPerimetro.nome}',
-    );
-  }
 }
 
-/// Representa a forma geometrica "círculo"
-class Circulo {
-  /// Construtor padrao, recebe o [raio] do círculo.
-  Circulo(this.nome, this.raio);
-
+/// Classe base abstrata para todas as formas geométricas
+abstract class FormaGeometrica {
   final String nome;
+
+  FormaGeometrica(this.nome);
+
+  double get area;
+  double get perimetro;
+}
+
+/// Implementação da classe Circulo com herança
+class Circulo extends FormaGeometrica {
   final double raio;
 
-  /// Retorna a area desse círculo
+  Circulo(String nome, this.raio) : super(nome);
+
+  @override
   double get area => math.pi * math.pow(raio, 2);
 
-  /// Retorna o perímetro desse círculo
+  @override
   double get perimetro => 2 * math.pi * raio;
 }
 
-/// Representa a forma geometrica "retângulo", forma geometrica de quatro lados
-/// e angulos retos (90 graus).
-class Retangulo {
-  /// Construtor padrao, recebe a [altura] e [largura] do retângulo
-  Retangulo(this.nome, this.altura, this.largura);
-
-  final String nome;
-  final double largura;
+/// Implementação da classe Retangulo com herança
+class Retangulo extends FormaGeometrica {
   final double altura;
+  final double largura;
 
-  /// Retorna a area desse círculo
+  Retangulo(String nome, this.altura, this.largura) : super(nome);
+
+  @override
   double get area => altura * largura;
 
-  /// Retorna o perímetro desse círculo
-  double get perimetro => (altura * 2) + (largura * 2);
+  @override
+  double get perimetro => 2 * (altura + largura);
 }
 
-/// Representa a forma geometrica "quadrado", que e um formato especial de
-/// retângulo, onde todos os lados possuem o mesmo tamanho.
-class Quadrado {
-  /// Construtor padrao, recebe o [lado] do quadrado
-  Quadrado(this.nome, this.lado);
-
-  final String nome;
+/// Implementação da classe Quadrado com herança
+class Quadrado extends FormaGeometrica {
   final double lado;
 
-  /// Retorna a area desse quadrado
+  Quadrado(String nome, this.lado) : super(nome);
+
+  @override
   double get area => lado * lado;
 
-  /// Retorna o perímetro desse quadrado
-  double get perimetro => lado * 4;
+  @override
+  double get perimetro => 4 * lado;
 }
 
-/// Compara caracteristicas de formas geometricas
+/// Implementação da classe Triangulo Equilátero com herança
+class TrianguloEquilatero extends FormaGeometrica {
+  final double lado;
+
+  TrianguloEquilatero(String nome, this.lado) : super(nome);
+
+  @override
+  double get area => (math.pow(3, 0.5) / 4) * math.pow(lado, 2);
+
+  @override
+  double get perimetro => 3 * lado;
+}
+
+/// Implementação da classe Triangulo Retângulo com herança
+class TrianguloRetangulo extends FormaGeometrica {
+  final double cateto1;
+  final double cateto2;
+
+  TrianguloRetangulo(String nome, this.cateto1, this.cateto2) : super(nome);
+
+  @override
+  double get area => (cateto1 * cateto2) / 2;
+
+  @override
+  double get perimetro => cateto1 + cateto2 + math.sqrt(cateto1 * cateto1 + cateto2 * cateto2);
+}
+
+/// Implementação da classe Pentágono Regular com herança
+class PentagonoRegular extends FormaGeometrica {
+  final double lado;
+
+  PentagonoRegular(String nome, this.lado) : super(nome);
+
+  @override
+  double get area => (5 * lado * lado) / (4 * math.tan(math.pi / 5));
+
+  @override
+  double get perimetro => 5 * lado;
+}
+
+/// Implementação da classe Hexágono Regular com herança
+class HexagonoRegular extends FormaGeometrica {
+  final double lado;
+
+  HexagonoRegular(String nome, this.lado) : super(nome);
+
+  @override
+  double get area => (3 * math.sqrt(3) * lado * lado) / 2;
+
+  @override
+  double get perimetro => 6 * lado;
+}
+
+/// Comparador utilizando polimorfismo
 class ComparadorFormasGeometricas {
-  /// Retorna o círculo com a maior area, ou [circuloA] caso as areas sejam
-  /// iguais
-  Circulo circuloDeMaiorArea(Circulo circuloA, Circulo circuloB) {
-    if (circuloA.area > circuloB.area) {
-      return circuloA;
-    } else if (circuloB.area > circuloA.area) {
-      return circuloB;
-    }
-    return circuloA;
+  FormaGeometrica maiorArea(FormaGeometrica a, FormaGeometrica b) {
+    return a.area >= b.area ? a : b;
   }
 
-  /// Retorna o círculo com o maior perímetro, ou [circuloA] caso os perímetros
-  /// sejam iguais
-  Circulo circuloDeMaiorPerimetro(Circulo circuloA, Circulo circuloB) {
-    if (circuloA.perimetro > circuloB.perimetro) {
-      return circuloA;
-    } else if (circuloB.perimetro > circuloA.perimetro) {
-      return circuloB;
-    }
-    return circuloA;
-  }
-
-  /// Retorna o retângulo com o maior perímetro, ou [retanguloA] caso os
-  /// perímetros sejam iguais
-  Retangulo retanguloDeMaiorPerimetro(Retangulo retanguloA, Retangulo retanguloB) {
-    if (retanguloA.perimetro > retanguloB.perimetro) {
-      return retanguloA;
-    } else if (retanguloB.perimetro > retanguloA.perimetro) {
-      return retanguloB;
-    }
-    return retanguloA;
-  }
-
-  /// Retorna o retângulo com o maior perímetro, ou [retanguloA] caso os
-  /// perímetros sejam iguais
-  Retangulo retanguloDeMaiorArea(Retangulo retanguloA, Retangulo retanguloB) {
-    if (retanguloA.area > retanguloB.area) {
-      return retanguloA;
-    } else if (retanguloB.area > retanguloA.area) {
-      return retanguloB;
-    }
-    return retanguloA;
+  FormaGeometrica maiorPerimetro(FormaGeometrica a, FormaGeometrica b) {
+    return a.perimetro >= b.perimetro ? a : b;
   }
 }
+
+
