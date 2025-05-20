b249c5804ec7c541b829b26bf8487214239399cb
Reinertjg <jgcosta.rei4@gmail.com>
2025-05-20 05:36:36 -0300

    Estrutura inicial para resolucao de exercicios diversos

---
+++ Flutter Training/draft/lib/main.dart
@@ -0,0 +1,148 @@
+import 'dart:convert';
+
+import 'package:flutter/material.dart';
+import 'dart:async';
+import 'package:http/http.dart' as http;
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatefulWidget {
+  const MyApp({super.key});
+
+  @override
+  State<MyApp> createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  List<String> listaProduto = List<String>.generate(
+    20,
+    (i) => 'Produto ${i + 1}',
+  );
+
+  Future<List<Produto>> _getProdutos() async {
+    Uri url = Uri.parse(
+      'https://api.json-generator.com/templates/SFhy78n33IvS/data?access_token=htth6zeba74eubpy3a2k5z5dr3z5lb5sbmqpw4qz',
+    );
+    print(url);
+
+    var response = await http.get(url);
+    var dados = json.decode(response.body) as List;
+
+    List<Produto> produtos = [];
+
+    dados.forEach((elemento) {
+      print(elemento['produto']);
+
+      Produto produto = Produto(
+        elemento['produto_id'],
+        elemento['produto'],
+        elemento['endereco'],
+        elemento['descricao'],
+        elemento['imagem'],
+        int.parse(elemento['quantidade']),
+        double.parse(elemento['preco']),
+      );
+
+      produtos.add(produto);
+    });
+    return produtos;
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(title: Text('List View')),
+        body: FutureBuilder(
+          future: _getProdutos(),
+          builder: (BuildContext context, AsyncSnapshot snapshot) {
+            if (snapshot.data == null) {
+              return Text('Carregando...');
+            } else {
+              return ListView.builder(
+                itemCount: snapshot.data.length,
+                itemBuilder: (context, indice) {
+                  return ListTile(
+                    leading: Image.network(
+                      'https://i.pinimg.com/736x/5a/a0/71/5aa07122296293d574c1ee4e8e1ab584.jpg',
+                    ),
+                    title: Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        Text(
+                          '${snapshot.data[indice].produto}',
+                          style: TextStyle(fontSize: 20),
+                        ),
+                        Text(
+                          'R\$ ${snapshot.data[indice].preco}',
+                          style: TextStyle(color: Colors.red, fontWeight: FontWeight.bold), 
+
+                        ),
+                      ],
+                    ),
+                    subtitle: Text(snapshot.data[indice].descricao.toString().substring(0, 30) + '...'),
+                    onTap: () {
+                      print(
+                        'O Produto selecionado foi ${listaProduto[indice]}, na posição $indice',
+                      );
+                    },
+                  );
+                },
+              );
+            }
+          },
+        ),
+      ),
+    );
+  }
+}
+
+class Produtos {
+  final List produtos;
+
+  Produtos(this.produtos);
+}
+
+class Produto {
+  final String produtoId;
+  final String produto;
+  final String endereco;
+  final String descricao;
+  final String imagem;
+  final int quantidade;
+  final double preco;
+
+  Produto(
+    this.produtoId,
+    this.produto,
+    this.endereco,
+    this.descricao,
+    this.imagem,
+    this.quantidade,
+    this.preco,
+  );
+
+  factory Produto.fromJson(Map<String, dynamic> json) {
+    return Produto(
+      json['produtoId'],
+      json['produto'],
+      json['endereco'],
+      json['descricao'],
+      json['imagem'],
+      json['quantidade'],
+      json['preco'],
+    );
+  }
+
+  Map<String, dynamic> toJson() => {
+    'produtoId': this.produtoId,
+    'produto': this.produto,
+    'endereco': this.endereco,
+    'descricao': this.descricao,
+    'imagem': this.imagem,
+    'quantidade': this.quantidade,
+    'preco': this.preco,
+  };
+}
