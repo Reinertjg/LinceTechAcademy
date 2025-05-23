286d388fe5f7d1d9344529c0c83f1b6bb8cdbe6b
Reinertjg <jgcosta.rei4@gmail.com>
2025-05-22 23:54:02 -0300

    Iniciando - Apf6_funcoes - Flutter Provider

---
+++ Flutter Training/apf6_funcoes/ap1/lib/telas/tela_inicial.dart
@@ -0,0 +1,37 @@
+import 'package:flutter/material.dart';
+import 'package:ap1/main.dart';
+
+class TelaInicial extends StatefulWidget {
+  const TelaInicial({super.key});
+
+  @override
+  State<TelaInicial> createState() => _TelaInicialState();
+}
+
+class _TelaInicialState extends State<TelaInicial> {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text('Tela Inicial'),
+          backgroundColor: Colors.deepOrange,
+        ),
+        body: Column(
+          children: [
+            ElevatedButton(onPressed: () {
+              Navigator.pushNamed(context, '/listagem');
+            }, child: Text('Listagem'),
+            ),
+            ElevatedButton(onPressed: () {
+              Navigator.pushNamed(context, '/listagem');
+            }, child: Text('Formulário'),
+            ),
+          ],
+
+
+        ),
+      ),
+    );
+  }
+}
