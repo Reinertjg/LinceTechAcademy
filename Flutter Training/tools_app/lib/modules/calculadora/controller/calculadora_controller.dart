7a8c21990dce628b05e928baed6f77cda5fdf063
joao.reinertcosta <jgcosta.rei4@gmail.com>
2025-06-06 16:59:54 -0300

    feat: refatora lógica da calculadora para suportar múltiplas operações e exibir resultados corretamente

--- Flutter Training/tools_app/lib/modules/calculadora/controller/calculadora_controller.dart
+++ Flutter Training/tools_app/lib/modules/calculadora/controller/calculadora_controller.dart
@@ -3,9 +3,12 @@
 import 'package:flutter/cupertino.dart';
 
 class CalculadoraController {
-  double _resultado = 0.0;
-  double valor = 0.0;
-  String calculo = '';
+  double primeiroValor = 0.0;
+  double segundoValor = 0.0;
+  double resultado = 0.0;
+  String operador = '';
+  bool novoNumero = true;
+  String hint = '0';
 
   final TextEditingController valorController = TextEditingController();
   final TextEditingController operacaoController = TextEditingController();
@@ -13,24 +16,84 @@ class CalculadoraController {
 
   void calcularResultado(String tecla) {
 
-    calculo += tecla;
+
     switch(tecla) {
       case 'AC':
-        valorController.clear();
+        primeiroValor = 0.0;
+        segundoValor = 0.0;
+        operador = '';
+        novoNumero = true;
+        valorController.text = '';
+        operacaoController.text = '';
+        hint = '0';
+        resultado = 0.0;
         break;
       case '+':
-        valor = double.tryParse(valorController.text) ?? 0.0;
-        operacaoController.text += valor.toString() + ' + ';
-        _resultado += valor;
-        print(valor);
+      case '-':
+      case '×':
+      case '÷':
+        if (valorController.text.isNotEmpty) {
+          primeiroValor = double.parse(valorController.text);
+          operador = tecla;
+          operacaoController.text += '$primeiroValor $operador';
+          resultado += primeiroValor;
+          hint = resultado.toString();
+          valorController.text = '';
+          novoNumero = true;
+        }
         break;
+
       case '=':
-        // Aqui você pode implementar a lógica para calcular o resultado final
-        // Por exemplo, se você estiver acumulando valores, você pode exibir o resultado
-        valorController.text = _resultado.toString();
+        if(operacaoController.text.endsWith('+')) {
+          operacaoController.text = '${operacaoController.text}$hint=';
+          resultado += hint.isNotEmpty ? double.parse(hint) : 0.0;
+        }
+        if(operacaoController.text.endsWith('=')) {
+          operacaoController.text = '${resultado}$hint=';
+          resultado += hint.isNotEmpty ? double.parse(hint) : 0.0;
+          hint = resultado.toString();
+        }
+        if (valorController.text.isNotEmpty && operador.isNotEmpty) {
+          segundoValor = double.parse(valorController.text);
+          print(operacaoController.text);
+
+
+          switch (operador) {
+            case '+':
+              resultado = primeiroValor + segundoValor;
+              break;
+            case '-':
+              resultado = primeiroValor - segundoValor;
+              break;
+            case '×':
+              resultado = primeiroValor * segundoValor;
+              break;
+            case '÷':
+              if (segundoValor != 0) {
+                resultado = primeiroValor / segundoValor;
+              } else {
+                // evitar divisão por zero
+                valorController.text = 'Erro';
+                operacaoController.text = '';
+                return;
+              }
+              break;
+          }
+
+          valorController.text = resultado.toString();
+          operacaoController.text = '$primeiroValor $operador $segundoValor =';
+          novoNumero = true;
+        }
         break;
+
+
       default:
-        valorController.text += tecla;
+        if (novoNumero) {
+          valorController.text = tecla;
+          novoNumero = false;
+        } else {
+          valorController.text += tecla;
+        }
         break;
     }
   }
