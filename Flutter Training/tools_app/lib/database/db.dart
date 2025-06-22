6b3c59c09a09e0f5c9811b733578a534fd1572d2
Reinertjg <jgcosta.rei4@gmail.com>
2025-06-22 17:27:28 -0300

    feat: improve database initialization and query methods for better performance

--- Flutter Training/tools_app/lib/database/db.dart
+++ Flutter Training/tools_app/lib/database/db.dart
@@ -7,40 +7,44 @@ import '../modules/conversor_unidades/models/tipo_conversor.dart';
 class DB {
   // Constructor privado
   DB._();
+
   // Instância única do banco de dados
   static final DB instance = DB._();
+
   // Banco de dados
   static Database? _database;
-  // Getter para o banco de dados
-  get database async {
-    if (_database != null) return database;
 
-    return await _initDatabase();
+  // Getter para o banco de dados
+  Future<Database> get database async {
+    if (_database != null) return _database!;
+    _database = await _initDatabase();
+    return _database!;
   }
 
   // Inicializa o banco de dados
   _initDatabase() async {
-    return await openDatabase (
+    return await openDatabase(
       join(await getDatabasesPath(), 'tools_app.db'),
       version: 1,
       onCreate: _onCreate,
     );
   }
 
-  _onCreate(db, versao) async {
+  Future<void> _onCreate(Database db, int version) async {
     await db.execute(_conversorUnidadesTable);
   }
 
+
   // Tabelas do banco de dados
   String get _conversorUnidadesTable => '''
-    CREATE TABLE $tableName (
-      $id INTERGER PRIMARY KEY AUTOINCREMENT,
-      $valorOrigem TEXT NOT NULL,
-      $tipoOrigem TEXT NOT NULL,
-      $valorDestino TEXT NOT NULL,
-      $tipoDestino TEXT NOT NULL,
-    );
-  ''';
+  CREATE TABLE $tableName (
+    $id INTEGER PRIMARY KEY AUTOINCREMENT,
+    $valorOrigem TEXT NOT NULL,
+    $tipoOrigem TEXT NOT NULL,
+    $valorDestino TEXT NOT NULL,
+    $tipoDestino TEXT NOT NULL
+  );
+''';
 
   static const String tableName = 'conversorUnidades';
   static const String id = 'id';
@@ -60,7 +64,6 @@ class DB {
 }
 
 class OperacaoUnidadesController {
-
   Future<void> insert(ConversorUnidadesModel unidades) async {
     final db = await DB.instance.database;
     final map = DB.toMap(unidades);
@@ -75,20 +78,18 @@ class OperacaoUnidadesController {
   Future<List<ConversorUnidadesModel>> select() async {
     final db = await DB.instance.database;
 
-    final List<Map<String, dynamic>> result = await db.query(
-        DB.tableName,
-    );
+    final List<Map<String, dynamic>> result = await db.query(DB.tableName);
 
     var list = <ConversorUnidadesModel>[];
 
     for (final item in result) {
       list.add(
-          ConversorUnidadesModel(
+        ConversorUnidadesModel(
           valorOrigem: item[DB.valorOrigem],
           tipoOrigem: item[DB.tipoOrigem],
           valorDestino: item[DB.valorDestino],
-          tipoDestino: item[DB.tipoDestino]
-          )
+          tipoDestino: item[DB.tipoDestino],
+        ),
       );
     }
 
