d807795d243b1945be794c039af5174f67c83e41
Reinertjg <jgcosta.rei4@gmail.com>
2025-06-25 23:33:32 -0300

    feat: enhance task management UI with animated opacity and improved task details

--- Flutter Training/taks_app/lib/main.dart
+++ Flutter Training/taks_app/lib/main.dart
@@ -1,11 +1,18 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(const MyApp());
+  runApp(MyApp());
 }
 
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
+class MyApp extends StatefulWidget {
+  MyApp({super.key});
+
+  @override
+  State<MyApp> createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  var opacity = true;
 
   @override
   Widget build(BuildContext context) {
@@ -15,19 +22,52 @@ class MyApp extends StatelessWidget {
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
       ),
       home: Scaffold(
+        backgroundColor: Colors.grey[300],
         appBar: AppBar(title: Text('Task App')),
-        body: ListView(
-          children: [
-            Task(
-              'Aprender Flutter, dart, bloc, provider, clean architecture, testes, etc',
-            ),
-            Task('Aprender Flutter'),
-            Task('Aprender Flutter'),
-            Task('Aprender Flutter'),
-            Task('Aprender Flutter'),
-            Task('Aprender Flutter'),
-          ],
+        body: AnimatedOpacity(
+          opacity: opacity ? 1 : 0,
+          duration: Duration(seconds: 2),
+          child: ListView(
+            children: [
+              Task(
+                'Flutter',
+                'https://cdn.prod.website-files.com/654366841809b5be271c8358/659efd7c0732620f1ac6a1d6_why_flutter_is_the_future_of_app_development%20(1).webp',
+                5,
+              ),
+              Task(
+                'Dart',
+                'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQUltGkCPo8TgI8EC7xoTk9OAGK_6DivdquIw&s',
+                3,
+              ),
+              Task(
+                'Video games',
+                'https://s2-techtudo.glbimg.com/b_unTeZIzl9wmcqoCwScjEepYR0=/0x0:755x426/984x0/smart/filters:strip_icc()/i.s3.glbimg.com/v1/AUTH_08fbf48bc0524877943fe86e43087e7a/internal_photos/bs/2021/R/a/Xav7B9QaaTHBE5s5KjTw/destaque-premium-beat.jpg',
+                1,
+              ),
+              Task(
+                'Academia',
+                'https://p2.trrsf.com/image/fget/cf/1200/1200/middle/images.terra.com/2024/07/04/1275438437-academia.jpg',
+                3,
+              ),
+              Task(
+                'Bike',
+                'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSVWONG3C9pN_HsnxohM6RZCHoILFCJ1Lt3hg&s',
+                2,
+              ),
+            ],
+          ),
         ),
+        floatingActionButton: FloatingActionButton(onPressed: () {
+          setState(() {
+            opacity = !opacity;
+          });
+        },
+          backgroundColor: Colors.blueAccent,
+          foregroundColor: Colors.white,
+          shape: RoundedRectangleBorder(
+            borderRadius: BorderRadius.circular(12),
+          ),
+        child: Icon(Icons.remove_red_eye_outlined),),
       ),
     );
   }
@@ -36,60 +76,174 @@ class MyApp extends StatelessWidget {
 //Trecho de código suprimido
 
 class Task extends StatefulWidget {
-    final String title;
+  final String title;
+  final String imageUrl;
+  final int difficulty;
 
-    const Task(this.title, {super.key});
+  const Task(this.title, this.imageUrl, this.difficulty, {super.key});
 
-    @override
-    State<Task> createState() => _TaskState();
-  }
+  @override
+  State<Task> createState() => _TaskState();
+}
 
-  class _TaskState extends State<Task> {
-    int level = 0;
-    @override
-    Widget build(BuildContext context) {
+class _TaskState extends State<Task> {
+  int level = 0;
+
+  @override
+  Widget build(BuildContext context) {
     return Padding(
       padding: const EdgeInsets.all(8.0),
       child: Stack(
         children: [
-          Container(color: Colors.blue, height: 140),
+          Container(
+            height: 140,
+            decoration: BoxDecoration(
+              color: Colors.blue,
+              borderRadius: BorderRadius.circular(10),
+            ),
+          ),
           Column(
             children: [
               Container(
-                color: Colors.white,
                 height: 100,
+                decoration: BoxDecoration(
+                  borderRadius: BorderRadius.circular(10),
+                  color: Colors.white,
+                  boxShadow: [
+                    BoxShadow(
+                      color: Colors.black45,
+                      offset: Offset(0, 2),
+                      blurRadius: 5,
+                      spreadRadius: 1,
+                    ),
+                  ],
+                ),
                 child: Row(
                   mainAxisAlignment: MainAxisAlignment.spaceBetween,
                   children: [
-                    Container(color: Colors.black26, width: 72, height: 100),
-                    SizedBox(
-                      width: 200,
-                      child: Text(
-                        widget.title,
-                        style: TextStyle(
-                          fontSize: 24,
-                          overflow: TextOverflow.ellipsis,
+                    Container(
+                      color: Colors.black26,
+                      width: 72,
+                      height: 100,
+                      child: Image.network(widget.imageUrl, fit: BoxFit.cover),
+                    ),
+                    Column(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        SizedBox(
+                          width: 200,
+                          child: Text(
+                            widget.title,
+                            style: TextStyle(
+                              fontSize: 24,
+                              overflow: TextOverflow.ellipsis,
+                            ),
+                          ),
+                        ),
+                        Row(
+                          children: [
+                            Icon(
+                              Icons.star,
+                              size: 15,
+                              color: (widget.difficulty >= 1)
+                                  ? Colors.blue
+                                  : Colors.blue[200],
+                            ),
+                            Icon(
+                              Icons.star,
+                              size: 15,
+                              color: (widget.difficulty >= 2)
+                                  ? Colors.blue
+                                  : Colors.blue[200],
+                            ),
+                            Icon(
+                              Icons.star,
+                              size: 15,
+                              color: (widget.difficulty >= 3)
+                                  ? Colors.blue
+                                  : Colors.blue[200],
+                            ),
+                            Icon(
+                              Icons.star,
+                              size: 15,
+                              color: (widget.difficulty >= 4)
+                                  ? Colors.blue
+                                  : Colors.blue[200],
+                            ),
+                            Icon(
+                              Icons.star,
+                              size: 15,
+                              color: (widget.difficulty >= 5)
+                                  ? Colors.blue
+                                  : Colors.blue[200],
+                            ),
+                          ],
+                        ),
+                      ],
+                    ),
+                    Padding(
+                      padding: const EdgeInsets.all(8.0),
+                      child: SizedBox(
+                        width: 50,
+                        height: 50,
+                        child: ElevatedButton(
+                          onPressed: () {
+                            // Implementar ação de completar tarefa
+                            setState(() {
+                              level++;
+                            });
+                          },
+                          style: ElevatedButton.styleFrom(
+                            backgroundColor: Colors.blueAccent,
+                            foregroundColor: Colors.white,
+                            shape: RoundedRectangleBorder(
+                              borderRadius: BorderRadius.circular(12),
+                            ),
+                            padding: EdgeInsets.all(0),
+                          ),
+                          child:
+                          Column(
+                            mainAxisAlignment: MainAxisAlignment.center,
+                            children: [
+                              Icon(
+                                Icons.arrow_drop_up,
+                                color: Colors.white,
+                                size: 35,
+                              ),
+                              Text(
+                                'UP',
+                                style: TextStyle(fontSize: 10),
+                              ),
+                            ],
+                          ),
                         ),
                       ),
-                    ),
-                    ElevatedButton(
-                      onPressed: () {
-                        // Implementar ação de completar tarefa
-                        setState(() {
-                          level++;
-                        });
-                      },
-                      style: ElevatedButton.styleFrom(
-                        backgroundColor: Colors.blueAccent,
-                        foregroundColor: Colors.white,
-                        shape: CircleBorder(),
-                      ),
-                      child: Icon(Icons.arrow_drop_up, color: Colors.white),
                     ),
                   ],
                 ),
               ),
-              Text('Nivel: $level', style: TextStyle(color: Colors.white)),
+              Padding(
+                padding: const EdgeInsets.all(10.0),
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    SizedBox(
+                      width: 200,
+                      child: LinearProgressIndicator(
+                        value: (widget.difficulty > 0)
+                            ? (level / widget.difficulty) / 5
+                            : 1,
+                        backgroundColor: Colors.white,
+                      ),
+                    ),
+                    Text(
+                      'Nivel: $level',
+                      style: TextStyle(color: Colors.white),
+                    ),
+                  ],
+                ),
+              ),
             ],
           ),
         ],
